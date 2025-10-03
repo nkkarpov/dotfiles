4502c24db8c7772188544bb157450640a2201868
Nikolai Karpov <nikolai.karpov@logiqai.com>
2025-10-02 17:19:13 -0700

    update setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl -LsSf https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -sS https://starship.rs/install.sh | sh
+curl https://install.duckdb.org | sh
 brew install --cask font-jetbrains-mono-nerd-font
 brew install visual-studio-code
 
